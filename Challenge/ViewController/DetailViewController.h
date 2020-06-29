c67039a37837887fb42a9cf263b5cea226b14607
swetadodiya <shweta.dodia@findnemo.in>
2020-06-29 09:41:31 -0600

    first commit

---
+++ Challenge/ViewController/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Challenge
+//
+//  Created by shweta dodiya on 2020-06-28.
+//  Copyright © 2020 shweta dodiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
